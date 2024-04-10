c4d3380abdc49513412f6fd6991abc7a89c52e1b
kuchbhimingwal <shubhammingi@gmail.com>
2024-04-09 23:41:24 -0400

    configured routes for the pages and tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Send from "./pages/Send";
+import Signin from "./pages/Sigin";
+import Signup from "./pages/Signup";
 function App() {
 
   return (
     <div>
-        Hello world
+        <BrowserRouter>
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />}/>
+            <Route path="/send" element={<Send />}/>
+            <Route path="/signin" element={<Signin />}/>
+            <Route path="/signuo" element={<Signup />}/>
+          </Routes>
+        </BrowserRouter>
     </div>
   )
 }
