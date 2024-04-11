309ecee2dc3a192f2c0b7db436a6c58b1422caa1
kuchbhimingwal <shubhammingi@gmail.com>
2024-04-11 00:41:59 -0400

    connected the backend for sign in

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,23 @@
 import React, { useState } from 'react'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function signup() {
+  const navigate = useNavigate()
   const [errors, setErrors] = useState('')
-  const clickHandler = ()=>{
-    
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const clickHandler = async()=>{
+    try {
+      const response = await axios.post('http://localhost:3000/api/v1/user/signin', {
+        username: email,
+        password: password
+      })
+      navigate("/dashboard")
+      console.log(response);
+    } catch (error) {
+      setErrors(error.response.data.message);
+    }
   }
   return (
     <div className=' flex min-h-screen items-center justify-center bg-gray-400'>
@@ -12,17 +26,17 @@ function signup() {
           <div className='text-center text-gray-400 p-2'>Enter your credentials to access your account</div>
           <div>
             <div className='p-2 font-bold'>Email</div>
-            <input type="text" placeholder='Write your email' className='border border-gray-200 rounded-md p-2 w-full' onChange={(e)=>{setFirstName(e.target.value)}}/>
+            <input type="text" placeholder='Write your email' className='border border-gray-200 rounded-md p-2 w-full' onChange={(e)=>{setEmail(e.target.value)}}/>
           </div>
           <div>
             <div className='p-2 font-bold'>Password</div>
             <input type="password" placeholder='Enter your Password' className='border border-gray-200 rounded-md p-2 w-full' onChange={(e)=>{setPassword(e.target.value)}}/>
           </div>
           <div>
-            <button className='w-full bg-black text-white rounded-md p-2 my-2' onClick={clickHandler}>Sign Up</button>
+            <button className='w-full bg-black text-white rounded-md p-2 my-2' onClick={clickHandler}>Sign In</button>
           </div>
           <div className='text-center text-red-400'>{errors}</div>
-          <div className='text-center'>Already have a account? <span className='underline cursor-pointer'>Sign In</span></div>
+          <div className='text-center'>Don't have a account? <span className='underline cursor-pointer'>Sign Up</span></div>
       </div>
     </div>
   )
